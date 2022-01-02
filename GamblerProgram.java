b5dfc7f7decf0e55e09955482e17663a48c4889b
AkshataHasabi <akshuh818@gmail.com>
2022-01-02 13:25:16 +0530

    ADDED:used random function to check win or loose

--- GamblerProgram.java
+++ GamblerProgram.java
@@ -5,5 +5,15 @@ public class GamblerProgram {
 	public static void main(String[] args) {
 		int startingTotalAmount=100;
 		int betsPerGame=1;
+		double winOrLoose=Math.floor(Math.random()*10)%2;
+		if(betsPerGame==winOrLoose) {
+			System.out.println("you won the game");
+			startingTotalAmount=startingTotalAmount+1;
+			System.out.println("After winning the game your total amount is:"+startingTotalAmount);
+		}else {
+			System.out.println("you loose the game");
+			startingTotalAmount=startingTotalAmount-1;
+			System.out.println("After loosing the game your total amount is:"+startingTotalAmount);
+		}
 	}
 }
