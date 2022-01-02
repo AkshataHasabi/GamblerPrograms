b88677140f7b02b8df6c7e30d2318d639ce6fd39
AkshataHasabi <akshuh818@gmail.com>
2022-01-02 13:03:13 +0530

    ADDED:prints total amount and bets per game

--- GamblerProgram.java
+++ GamblerProgram.java
@@ -3,6 +3,7 @@ package com.bridgelab.gamblerprograms;
 public class GamblerProgram {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Gambler Progamms");
+		int startingTotalAmount=100;
+		int betsPerGame=1;
 	}
 }
